fde0a2c1a9176bb4fbe65449ab361d27924544cd
yeefea <yifei.shen@yahoo.com>
2022-06-24 22:44:09 +0800

    update: demo

--- cmd/main.go
+++ cmd/main.go
@@ -14,8 +14,34 @@ func init() {
 }
 
 func main() {
-	re := "Hello, regular expression .+\\!"
+	re := `Hello, regular expression .+!`
 	g, err := regstr.Parse(re, syntax.Perl)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 10; i++ {
+		randstr, err := g.Gen()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(randstr)
+	}
+
+	js := `\{".+": ".+"\}`
+	g, err = regstr.Parse(js, syntax.Perl)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 10; i++ {
+		randstr, err := g.Gen()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(randstr)
+	}
+
+	sql := `INSERT INTO some_table\(col1,col2\) values \('.+','.+'\)`
+	g, err = regstr.Parse(sql, syntax.Perl)
 	if err != nil {
 		panic(err)
 	}
